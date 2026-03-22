4b5e584010d50ba633b574a96dbb8309ca78b30e
sarva1210 <sarva.vish77@gmail.com>
2026-03-22 17:07:46 +0530

    integrated layout

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        blood: "#9F0A10",
+        dark: "#000000",
+        damask: "#220C12",
+        rose: "#430102",
+        gold: "#C1914F",
+        light: "#EBE0C2",
+      },
+    },
+  },
+  plugins: [],
+};
