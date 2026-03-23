13e920a67aab6f8a355ce9eec04a16764f6fc017
sarva1210 <sarva.vish77@gmail.com>
2026-03-23 19:28:19 +0530

    integrating converter

--- frontend/src/Components/converter/SkeletonCard.jsx
+++ frontend/src/Components/converter/SkeletonCard.jsx
@@ -1,4 +1,5 @@
 export default function SkeletonCard() {
+  
   return (
     <div className="bg-card p-6 rounded-xl animate-pulse">
       <div className="h-4 bg-gray-300 rounded w-3/4 mb-4"></div>
