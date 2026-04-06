c610ab3fe2bdda65d84538f67455610618b846c5
sarva1210 <sarva.vish77@gmail.com>
2026-04-06 22:20:31 +0530

    integrated time feature

--- frontend/src/Utils/timeAgo.js
+++ frontend/src/Utils/timeAgo.js
@@ -1,6 +1,5 @@
 export const timeAgo = (date) => {
   const seconds = Math.floor((new Date() - new Date(date)) / 1000);
-
   const intervals = { d: 86400, h: 3600, m: 60 };
 
   for (let key in intervals) {
