f34b9a73b0badd04dcd5e17b2209c9179e52e56f
sarva1210 <sarva.vish77@gmail.com>
2026-05-02 20:16:27 +0530

    ai model

--- backend/src/Services/Ai/models.js
+++ backend/src/Services/Ai/models.js
@@ -8,6 +8,7 @@ export const geminiModel = new ChatGoogleGenerativeAI({
     apiKey: process.env.GEMINI_API_KEY,
 });
 
+//mistral model
 export const mistralModel = new ChatMistralAI({
     model: "mistral-medium-latest",
     apiKey: process.env.MISTRAL_API_KEY,
