c39174b1fe48a1853add6546c1ada1ad7bf955ea
sarva1210 <sarva.vish77@gmail.com>
2026-06-06 13:48:48 +0530

    progressbar

--- frontend/src/Components/converter/ProgressBar.jsx
+++ frontend/src/Components/converter/ProgressBar.jsx
@@ -1,11 +1,7 @@
 export default function ProgressBar({ progress }) {
-  
   return (
     <div className="bg-white h-3 rounded-full overflow-hidden">
-      <div
-        className="bg-primary h-3 transition-all duration-500"
-        style={{ width: `${progress}%` }}
-      />
+      <div className="bg-primary h-3 transition-all duration-500" style={{ width: `${progress}%` }}/>
     </div>
   );
 }
